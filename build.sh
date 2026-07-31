ffef1b7a84177cd58d6141de01bfa0e1f4d3161d
Wandererre <manjunath0083@kgpian.iitkgp.ac.in>
2026-07-31 15:13:35 +0000

    Add -lhiredis_ssl -lssl -lcrypto to build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Compiling odxt-cli..."
-g++ -std=c++17 -O2 -maes -march=native -mavx512f -mavx512vl -fpermissive \
+g++ -std=c++17 -O2 -maes -mavx2 -msse4.1 -fpermissive \
 odxt_cli.cpp aes.cpp rawdatautil.cpp ecc_x25519.cpp \
 ./c/blake_hash.cpp ./c/blake3.c ./c/blake3_dispatch.c ./c/blake3_portable.c \
-./c/blake3_avx2.c ./c/blake3_avx512.c ./c/blake3_sse2.c ./c/blake3_sse41.c \
+./c/blake3_avx2.c ./c/blake3_sse2.c ./c/blake3_sse41.c \
 odxt_main_single_thread.cpp utils.cpp main_single_thread.cpp \
--lgmpxx -lgmp -lredis++ -lhiredis -lpthread -o odxt-cli
+-lgmpxx -lgmp -lredis++ -lhiredis -lhiredis_ssl -lssl -lcrypto -lpthread -o odxt-cli
 echo "Build complete! Run 'python3 main.py' to start the server."
